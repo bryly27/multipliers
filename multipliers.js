e78d0804fd9a0a2a1c02e86dba803d744d8ec9a2
Bryant Yu <bryly27@sbcglobal.net>
2019-01-30 00:43:23 -0800

    Update multipliers.js

--- multipliers.js
+++ multipliers.js
@@ -9,7 +9,7 @@ function findMultipliers(arr) {
   });
 
   if (arr.length === 5) {
-    return arr[0] * arr[1] * arr[2] * arr[3] * arr[4]
+    return arr[0] * arr[1] * arr[2] * arr[3] * arr[4];
   } else {
     return Math.max(
       arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 3] * arr[arr.length - 4] * arr[arr.length - 5],
